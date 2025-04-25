eefd6d023c89a30c9ea117161c9a00a362105c4e
kkoryax <orachun.wsst@gmail.com>
2025-04-25 20:42:58 +0700

    installed package bcrypt, mongoose, jsonwebtoken

--- api/title.js
+++ api/title.js
@@ -3,5 +3,14 @@ import express from "express";
 const router = express.Router();
 
 export default(db) => {
+    //Create GET
+    router.get("/titles", (req, res) => {
+        const {name, description, picture } = req.body
+        try {
+
+        } catch (error) {
+            
+        }
+    })
     return router;
 }
