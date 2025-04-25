a54c43ac8d70b3f3dbf759ff8554661f8a50f143
kkoryax <orachun.wsst@gmail.com>
2025-04-25 20:54:20 +0700

    added connection to Mongo Database

--- server.js
+++ server.js
@@ -7,7 +7,19 @@ const app = express();
 
 const PORT = process.env.PORT
 
+//CREATE Middleware to parse JSON to JavaScript Object
 app.use(express.json());
+
+//CONNECT to MongoDB
+(async()=> {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log(`Connecting to MongoDB successfully ✅`);
+    } catch (err) {
+        console.error(`Database Connection failed: ${err}`);
+        process.exit(1);
+    }
+})();
 
 app.listen(PORT, () =>{
     console.log(`Server now running on http://localhost:${PORT}`)
