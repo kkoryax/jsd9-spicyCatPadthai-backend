9c19ca092377868559347a65a67eedc1d80e6315
Varinthorn <earnmaha@hotmail.com>
2025-04-26 22:41:13 +0700

    added the user route

--- api/users.js
+++ api/users.js
@@ -1,12 +1,36 @@
 import express from "express";
-import { getAllUsers, registerUser } from "./controllers/userControllers.js";
+import { getAllUsers, GetUserById, registerUser, loginUser, updateUser, deleteUser} from "./controllers/userControllers.js";
+
 
 const router = express.Router();
+
 
 //Get all users
 router.get("/auth/get-all-users", getAllUsers);
 
+
+//Get user by Id
+router.post("/auth/user/:id", GetUserById);
+
+
 //Register a user
 router.post("/auth/register", registerUser);
+
+
+//login
+router.post("/auth/login", loginUser);
+
+
+//Update user (some fields)
+router.patch("/auth/user/:id", updateUser);
+
+
+//Update user (All fields)
+router.put("/auth/user/:id", updateUser);
+
+
+//Delete User
+router.delete("/auth/user/:id", deleteUser);
+
 
 export default router;
