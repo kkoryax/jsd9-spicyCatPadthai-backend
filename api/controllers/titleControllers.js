d3511dae7b1603c895c591d94e61c112cd00d85b
kkoryax <orachun.wsst@gmail.com>
2025-04-27 13:40:14 +0700

    added create new title in titleControllers

--- api/controllers/titleControllers.js
+++ api/controllers/titleControllers.js
@@ -7,12 +7,40 @@ export const getAllTitles = async(req, res) => {
         res.json({
             error: false,
             title
-        })
+        });
     } catch (err) {
         res.status(500).json({
             error: true,
             message: "Failed to fetch all titles",
             details: err.message
-        })
+        });
     }
- }
+};
+
+ //CREATE new title
+ export const createNewTitle = async(req, res) => {
+    const { name, description } = req.body
+    if(!name || !description) {
+        return res.status(400).json({
+            error: true,
+            message: "All fields are required"
+        });
+    }
+    try {
+        const title = new Title ({
+            name,
+            description
+        });
+        await title.save();
+
+        res.status(201).json({
+            error: false,
+            message: "Title created successfully"
+        })
+    } catch (err) {
+        res.status(500).json({
+            error: false,
+            message: "Server error"
+        });
+    }
+};
