64c45718e6f69f1ec383a0f30dbf32570ff86822
kkoryax <orachun.wsst@gmail.com>
2025-05-09 12:11:54 +0700

    hotfix(auth): updated token for authentication and updated profile route

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,6 @@ import jwt from "jsonwebtoken"
 
 export const authUser = async(req, res, next) => {
     const token = req.cookies?.token
-    console.log(token)
     if(!token) {
         return res.json({
             success: false,
