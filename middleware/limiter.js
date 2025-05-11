4173a7f273ff6840d58826be71499dd9580da509
atichapre <aticha.pre@gmail.com>
2025-05-12 01:25:41 +0700

    update limiter

--- middleware/limiter.js
+++ middleware/limiter.js
@@ -1,8 +1,8 @@
 import rateLimit from "express-rate-limit";
 
 const limiter = rateLimit({
-  // windowMs: 15 * 60 * 1000, // 15 minutes
-  windowMs: 60 * 1000,
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  // windowMs: 60 * 1000,
   max: 1000,                 // limit each IP to 100 requests
   standardHeaders: true,
   legacyHeaders: false,
